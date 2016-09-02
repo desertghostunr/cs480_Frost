c3aff5e0286cf2d8d5b5829468fdeb21188e5903
desertghostunr <desertghost@nevada.unr.edu>
2016-09-02 11:04:48 -0700

    Created a 'clean' repository with proper naming convention

---
+++ PA1/include/object.h
@@ -0,0 +1,30 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float angle;
+    //added
+    float orbitalAngle; // Added by Andrew Frost 09/01/2016
+
+};
+
+#endif /* OBJECT_H */
